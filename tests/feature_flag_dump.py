aa860e8ead76bc144a7d6cea0038ccfc97643f30
Jeff Schnitter <jeff.schnitter@cortex.io>
2024-06-26 17:18:05 -0700

    more debugging

--- tests/feature_flag_dump.py
+++ tests/feature_flag_dump.py
@@ -10,14 +10,18 @@ h = {
 
 url = os.getenv('CORTEX_BASE_URL') + "/api/internal/v1/cortex/preferences"
 print("ff url = " + url)
+print("key = " + os.getenv('CORTEX_API_KEY'))
 
 try:
+    print("here in the try")
     r = requests.get(url, headers=h)
     r.raise_for_status()
 except requests.exceptions.RequestException as e:
+    print("here in the except")
     print(e.response.text)
     sys.exit(1)
 
+print("outside the try")
 print("feature flags = " + r.text)
 f = open(file, "w")
 f.write(r.text)
