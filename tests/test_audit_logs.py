f213e720224bce269281aa7953aef1a9758873df
Jeff Schnitter <jeff.schnitter@cortex.io>
2025-01-04 16:39:45 -0800

    Disable audit-logs tests until CET-15982 is resolved

--- tests/test_audit_logs.py
+++ tests/test_audit_logs.py
@@ -1,5 +1,6 @@
 from common import *
 
+@pytest.mark.skip(reason="Disabled until CET-15982 is resolved.")
 def test(capsys):
     response = cli_command(capsys, ["audit-logs", "get",])
     assert (len(response['logs']) > 0)
--- tests/test_audit_logs_end_date.py
+++ tests/test_audit_logs_end_date.py
@@ -1,5 +1,6 @@
 from common import *
 
+@pytest.mark.skip(reason="Disabled until CET-15982 is resolved.")
 def test(capsys):
     end_date = today()
     response = cli_command(capsys, ["audit-logs", "get", "-e", end_date])
