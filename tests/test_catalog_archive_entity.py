2007ed5448c46f9cfb6870987874eec74db970f9
Jeff Schnitter <jeff.schnitter@cortex.io>
2025-08-26 14:00:34 -0700

    chore: test uses incorrect entity tag

--- tests/test_catalog_archive_entity.py
+++ tests/test_catalog_archive_entity.py
@@ -1,7 +1,7 @@
 from tests.helpers.utils import *
 
 def test():
-    cli(["catalog", "archive", "-t", "archive-entity"])
+    cli(["catalog", "archive", "-t", "cli-test-archive-entity"])
 
-    response = cli(["catalog", "details", "-t", "archive-entity"])
+    response = cli(["catalog", "details", "-t", "cli-test-archive-entity"])
     assert response['isArchived'] == True, "isArchived attribute should be true"
